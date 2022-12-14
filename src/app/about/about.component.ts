1313d5514184d4dc067ee08a737ce4f8c3383c87
jbellon <josh@joshbellon.com>
2022-12-13 21:37:04 -0800

    Finished fourth video 'What is an RxJs Observable?'

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { fromEvent, interval, timer } from 'rxjs';
 
 @Component({
   selector: 'about',
@@ -10,23 +11,43 @@ export class AboutComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    document.addEventListener('click', evt => {
 
-      console.log(evt);
+    //The below code was used to show what data streams can be 
+    //(the click events below are a type of stream, the interval is a continuous stream, and the timeout is a stream with a single value) 
+    //The nested code below shows how multiple data streams can overlap creating confusing code and confusing results
+    // document.addEventListener('click', evt => {
 
-      setTimeout(() => {
-        console.log('Timeout has elapsed');
+    //   console.log(evt);
 
-        let counter = 0;
+    //   setTimeout(() => {
+    //     console.log('Timeout has elapsed');
 
-        setInterval(() => {
-          console.log(counter);
-          counter++;
-        }, 1000);
+    //     let counter = 0;
 
-      }, 3000);
-    });
+    //     setInterval(() => {
+    //       console.log(counter);
+    //       counter++;
+    //     }, 1000);
 
+    //   }, 3000);
+    // });
+
+    //THIS CODE WILL DECLARE A STREAM TEMPLATE (OBSERVABLE) REPEATING AT AN INTERVAL OF EVERY 1000 MS
+    // THE $ SIGN IS USED TO DECLARE AN OBSERVABLE
+    //const interval$ = interval(1000)
+
+    //THIS CODE DECLARES A STREAM TEMPLATE (OBSERVABLE) THAT WILL START AFTER 3000 MS AND THEN REPEAT EVERY 1000 MS
+    //THE $ SIGN IS USED TO DECLARE AN OBSERVABLE
+    const interval$ = timer(3000, 1000);
+
+    //SUBSCRIBING TO AN OBSERVABLE CREATES AN INSTANCE OF THAT DATA STREAM
+    interval$.subscribe(val => console.log('stream 1 ' + val));
+
+    interval$.subscribe(val => console.log('stream 2 ' + val));
+
+    const click$ = fromEvent(document, 'click');
+
+    click$.subscribe(evt => console.log(evt));
   }
 
 }
