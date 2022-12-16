9d7189cbd15754645dc049eadaa72f432567d2ed
jbellon <josh@joshbellon.com>
2022-12-15 16:25:46 -0800

    finished lesson 'setting up the course component'

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -1,6 +1,6 @@
-import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
-import {ActivatedRoute} from "@angular/router";
-import {Course} from "../model/course";
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { ActivatedRoute } from "@angular/router";
+import { Course } from "../model/course";
 import {
     debounceTime,
     distinctUntilChanged,
@@ -13,8 +13,9 @@ import {
     withLatestFrom,
     concatAll, shareReplay
 } from 'rxjs/operators';
-import {merge, fromEvent, Observable, concat} from 'rxjs';
-import {Lesson} from '../model/lesson';
+import { merge, fromEvent, Observable, concat } from 'rxjs';
+import { Lesson } from '../model/lesson';
+import { createHttpObservable } from '../common/util';
 
 
 @Component({
@@ -24,9 +25,8 @@ import {Lesson} from '../model/lesson';
 })
 export class CourseComponent implements OnInit, AfterViewInit {
 
-
-    course$: Observable<Course>;
-    lessons$: Observable<Lesson[]>;
+    course$: Observable<any>;
+    lessons$: Observable<any>;
 
 
     @ViewChild('searchInput', { static: true }) input: ElementRef;
@@ -40,8 +40,12 @@ export class CourseComponent implements OnInit, AfterViewInit {
 
         const courseId = this.route.snapshot.params['id'];
 
+        this.course$ = createHttpObservable(`/api/courses/${courseId}`);
 
-
+        this.lessons$ = createHttpObservable(`/api/lessons?courseId=${courseId}&pageSize=100`)
+            .pipe(
+                map(res => res['payload'])
+            );
     }
 
     ngAfterViewInit() {
