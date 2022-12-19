9de1e891e7081301b11e3ffe1bdf051249875860
jbellon <josh@joshbellon.com>
2022-12-19 14:42:33 -0800

    finished lesson 'rxjs error handling'

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -50,7 +50,18 @@ export class HomeComponent implements OnInit {
         const courses$ = http$.pipe(
             tap(() => console.log('HTTP Request Executed')),
             map(res => res['payload']),
-            shareReplay()
+            shareReplay(),
+            catchError(err => of([
+                {
+                    id: 0,
+                    description: "RxJs In Practice Course",
+                    iconUrl: 'https://s3-us-west-1.amazonaws.com/angular-university/course-images/rxjs-in-practice-course.png',
+                    courseListIcon: 'https://angular-academy.s3.amazonaws.com/main-logo/main-page-logo-small-hat.png',
+                    longDescription: "Understand the RxJs Observable pattern, learn the RxJs Operators via practical examples",
+                    category: 'BEGINNER',
+                    lessonsCount: 10
+                }
+            ]))
         );
 
         this.beginnerCourses$ = courses$.pipe(map(courses => courses.filter(course => course.category == 'BEGINNER')));
